9f9f210e349e7ef1373936f7a0a58fd946b5519d
Joris <jorisvanvugt@live.nl>
2015-04-19 02:39:13 +0200

    Actually add hints

---
+++ Assets/Scripts/RespawnScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnScript : MonoBehaviour 
+{
+    private Vector3 spawnPosition;
+    private Rigidbody rb;
+
+	void Start() 
+	{
+        rb = GetComponent<Rigidbody>();
+        spawnPosition = transform.position;
+	}
+
+    public void Respawn()
+    {
+        transform.position = spawnPosition;
+        if(rb != null)
+            rb.velocity = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawner : MonoBehaviour 
+{
+
+	void OnTriggerEnter(Collider col)
+    {
+        RespawnScript rs = col.GetComponent<RespawnScript>();
+        if(rs != null)
+        {
+            rs.Respawn();
+        }
+    }
+}
